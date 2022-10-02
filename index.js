ddb8d384f464ecb604bd6a834de22a1f7c0aa25d
Paulo Hernane <paulo@mobixtec.com>
2022-10-02 18:16:58 -0300

    create script

---
+++ index.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+setInterval(async () => {
+  try {
+    const { data } = await axios(
+      'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json',
+      {
+        headers: {
+          referer: 'https://resultados.tse.jus.br/oficial/app/index.html',
+        },
+      }
+    );
+
+    const { hg: horas, cand: candidatos } = data;
+
+    console.log(`------------ ${horas} ------------`);
+    console.table(
+      candidatos.map(
+        ({ n: Numero, nm: Nome, vap: Votos, pvap: Porcentagem }) => {
+          return {
+            Numero,
+            Nome,
+            Votos,
+            Porcentagem,
+          };
+        }
+      )
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}, 1 * 60 * 1000);
